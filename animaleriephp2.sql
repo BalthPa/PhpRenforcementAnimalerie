b854a4754a55743ed3837d75f21780cebb64890a
Prameet <47789905+pommet99@users.noreply.github.com>
2020-05-03 22:55:50 +0200

    Add files via upload

---
+++ animaleriephp2.sql
@@ -0,0 +1,219 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 03, 2020 at 08:50 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `animaleriephp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `animal`
+--
+
+DROP TABLE IF EXISTS `animal`;
+CREATE TABLE IF NOT EXISTS `animal` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `race` varchar(255) NOT NULL,
+  `taille` int(11) NOT NULL,
+  `poids` int(11) NOT NULL,
+  `age` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `animal`
+--
+
+INSERT INTO `animal` (`id`, `nom`, `type`, `race`, `taille`, `poids`, `age`) VALUES
+(2, 'Rex', 'Cocker', 'Chien', 102, 18, 5),
+(3, 'Coco', 'Norman', 'Lapin', 26, 3, 2),
+(4, 'Nemo', 'Poisson Clown', 'Poisson', 6, 0, 1),
+(5, 'Mistigris', 'Chartreux', 'Chat', 34, 8, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commande`
+--
+
+DROP TABLE IF EXISTS `commande`;
+CREATE TABLE IF NOT EXISTS `commande` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(100) NOT NULL,
+  `date` datetime NOT NULL,
+  `montant` int(11) NOT NULL,
+  `etat` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dons`
+--
+
+DROP TABLE IF EXISTS `dons`;
+CREATE TABLE IF NOT EXISTS `dons` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `montant` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `dons`
+--
+
+INSERT INTO `dons` (`id`, `nom`, `montant`, `date`) VALUES
+(1, 'MAnraj', 6, '2020-05-03 00:00:00'),
+(2, 'Manraj', 4, '2020-05-03 00:00:00'),
+(3, 'zzzz', 44, '2020-05-03 00:00:00'),
+(4, 'zzzz', 44, '2020-05-03 00:00:00'),
+(5, 'zzzz', 44, '2020-05-03 00:00:00'),
+(6, 'zzzz', 44, '2020-05-03 00:00:00'),
+(7, 'zzzz', 44, '2020-05-03 00:00:00'),
+(8, 'zzzz', 44, '2020-05-03 00:00:00'),
+(9, 'zzzz', 44, '2020-05-03 00:00:00'),
+(10, 'zzzz', 44, '2020-05-03 00:00:00'),
+(11, 'zzzz', 44, '2020-05-03 00:00:00'),
+(12, 'zzzz', 44, '2020-05-03 00:00:00'),
+(13, 'zzzz', 44, '2020-05-03 00:00:00'),
+(14, 'zzzz', 44, '2020-05-03 00:00:00'),
+(15, 'zzzz', 44, '2020-05-03 00:00:00'),
+(16, 'zzzz', 44, '2020-05-03 00:00:00'),
+(17, 'zzzz', 44, '2020-05-03 00:00:00'),
+(18, 'zzzz', 44, '2020-05-03 00:00:00'),
+(19, 'zzzz', 44, '2020-05-03 00:00:00'),
+(20, 'zzzz', 44, '2020-05-03 00:00:00'),
+(21, 'zzzz', 44, '2020-05-03 00:00:00'),
+(22, 'zzzz', 44, '2020-05-03 00:00:00'),
+(23, 'zzzz', 44, '2020-05-03 00:00:00'),
+(24, 'zzzz', 44, '2020-05-03 00:00:00'),
+(25, 'zzzz', 44, '2020-05-03 00:00:00'),
+(26, 'zzzz', 44, '2020-05-03 00:00:00'),
+(27, 'zzzz', 44, '2020-05-03 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `panier`
+--
+
+DROP TABLE IF EXISTS `panier`;
+CREATE TABLE IF NOT EXISTS `panier` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `produit_id` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  `montant` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `produit_id` (`produit_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produit`
+--
+
+DROP TABLE IF EXISTS `produit`;
+CREATE TABLE IF NOT EXISTS `produit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `animal` varchar(100) NOT NULL,
+  `prix` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `produit`
+--
+
+INSERT INTO `produit` (`id`, `nom`, `type`, `animal`, `prix`, `stock`) VALUES
+(1, 'Arbre a chat', 'Chat', 'Chat', 50, 4),
+(2, 'Os a chien', 'Chien', 'Chien', 20, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+CREATE TABLE IF NOT EXISTS `reservation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `animal_id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `date_rdv` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `animal_id` (`animal_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `prenom` varchar(100) NOT NULL,
+  `adresse` text NOT NULL,
+  `ville` varchar(100) NOT NULL,
+  `cp` int(11) NOT NULL,
+  `mail` text NOT NULL,
+  `tel` int(11) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `panier`
+--
+ALTER TABLE `panier`
+  ADD CONSTRAINT `panier_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `panier_ibfk_2` FOREIGN KEY (`produit_id`) REFERENCES `produit` (`id`);
+
+--
+-- Constraints for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`animal_id`) REFERENCES `animal` (`id`),
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
